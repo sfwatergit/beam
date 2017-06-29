ad90e321997c9b61ddbd14a7ba645114741ba669
mr-asif-nor <muhammad.asif1.northstar@gmail.com>
2017-06-29 23:31:37 +0500

    Added the exception class for our simulation in the case of event timestamp smaller than the last PhysSimTimeSyncEvent timestamp.

---
+++ src/main/java/beam/playground/exceptions/InvalidEventTime.java
@@ -0,0 +1,12 @@
+package beam.playground.exceptions;
+
+/**
+ * Created by asif on 6/29/2017.
+ */
+public class InvalidEventTime extends Exception{
+
+    @Override
+    public String getMessage() {
+        return "The timestamp for the event is smaller than the last PhysSyncTimeEvent timestamp";
+    }
+}
